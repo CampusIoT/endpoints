e8fd317f35570c0c3c3fc253e998c9f8248952f9
Didier Donsez <didier.donsez@gmail.com>
2019-10-28 08:39:56 +0100

    add LCD and LED with cpp macro

--- sodaq/Sodaq_OTAA_temp.ino
+++ sodaq/Sodaq_OTAA_temp.ino
@@ -1,6 +1,23 @@
+// SODAQ Explorer shetch with :
+// * OTAA or ABP network activation
+// * onboard temperature sensor
+// * Grove LCD Display (on I2C socket)
+// * RGB led
+
 // From https://support.sodaq.com/sodaq-one/lorawan/
 
 #include <Sodaq_RN2483.h>
+
+// Use OTAA, comment to use ABP
+#define OTAA
+
+// Use LCD, comment to disable lcd
+#define GROVE_RGB_LCD  
+
+#ifdef GROVE_RGB_LCD
+// Refer to http://wiki.seeedstudio.com/Grove-LCD_RGB_Backlight/ for installing the library
+#include <rgb_lcd.h>
+# endif
 
 #define debugSerial SerialUSB
 #define loraSerial Serial2
@@ -15,16 +32,35 @@
 
 // #define PIR_MOTION_PIN 9
 
-// TODO add #ifdef for OTAA/ABP in order to shrink the firmware
-//Use OTAA, set to false to use ABP
-bool OTAA = true;
 
-// ABP
-// USE YOUR OWN KEYS!
+
 const uint8_t devAddr[4] =
 {
     0x00, 0x00, 0x00, 0x00
 };
+
+// With using the GetHWEUI() function the HWEUI will be used
+uint8_t DevEUI[8] =
+{
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+#ifdef OTAA
+
+// OTAA
+const uint8_t AppEUI[8] =
+{
+  0x0c, 0xaf, 0xcb, 0x00, 0x00, 0xff, 0xff, 0xff
+};
+
+// 09cf4f3c0400123428aed2a6abf71588
+const uint8_t AppKey[16] =
+{
+0x09, 0xcf, 0x4f, 0x3c, 0x04, 0x00, 0x12, 0x34,
+0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88
+};
+
+#else
 
 // USE YOUR OWN KEYS!
 const uint8_t appSKey[16] =
@@ -40,28 +76,27 @@ const uint8_t nwkSKey[16] =
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-// OTAA
-// With using the GetHWEUI() function the HWEUI will be used
-uint8_t DevEUI[8] =
-{
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
+#endif
 
-const uint8_t AppEUI[8] =
-{
-  0x0c, 0xaf, 0xcb, 0x00, 0x00, 0xff, 0xff, 0xff
-};
+#ifdef GROVE_RGB_LCD
+rgb_lcd lcd;
 
-// 0400123428aed2a6abf7158809cf4f3c
-const uint8_t AppKey[16] =
-{
-0x04, 0x00, 0x12, 0x34, 0x28, 0xae, 0xd2, 0xa6,
-0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
-};
+#define WHITE 255,255,255
+#define BLACK 0,0,0
+#define RED 255,0,0
+#define GREEN 0,255,0
+#define BLUE 0,0,255
+
+#endif
 
 void setup()
 {
   delay(1000);
+#ifdef GROVE_RGB_LCD
+  lcd.begin(16, 2);
+  lcd.setRGB(WHITE);
+  lcd.print("LORA");
+#endif
 
   while ((!debugSerial) && (millis() < 10000)){
     // Wait 10 seconds for debugSerial to open
@@ -84,27 +119,41 @@ void setup()
   for (uint8_t i = 0; i < sizeof(DevEUI); i++) {
     debugSerial.print((char)NIBBLE_TO_HEX_CHAR(HIGH_NIBBLE(DevEUI[i])));
     debugSerial.print((char)NIBBLE_TO_HEX_CHAR(LOW_NIBBLE(DevEUI[i])));
+#ifdef GROVE_RGB_LCD
+    lcd.print((char)NIBBLE_TO_HEX_CHAR(HIGH_NIBBLE(DevEUI[i])));
+    lcd.print((char)NIBBLE_TO_HEX_CHAR(LOW_NIBBLE(DevEUI[i])));
+#endif
   }
   debugSerial.println();  
 
   setupLoRa();
 }
 
-void setupLoRa(){
-  if(!OTAA){
-    // ABP
-    setupLoRaABP();
-  } else {
-    //OTAA
-    setupLoRaOTAA();
+#ifdef OTAA
+void setupLoRaOTAA(){
+  if (LoRaBee.initOTA(loraSerial, DevEUI, AppEUI, AppKey, true))
+  {
+    debugSerial.println("Network connection successful.");
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+#ifdef GROVE_RGB_LCD
+    lcd.setCursor(0, 1);
+    lcd.setRGB(GREEN);
+    lcd.print("OTAA success");
+#endif
   }
-  // Uncomment this line to for the RN2903 with the Actility Network
-  // For OTAA update the DEFAULT_FSB in the library
-  // LoRaBee.setFsbChannels(1);
-
-  LoRaBee.setSpreadingFactor(9);
+  else
+  {
+    debugSerial.println("Network connection failed!");
+#ifdef GROVE_RGB_LCD
+    lcd.setCursor(0, 1);
+    lcd.setRGB(RED);
+    lcd.print("OTAA failed");  
+#endif
+  }
+  delay(1000);
 }
-
+#else
 void setupLoRaABP(){  
   if (LoRaBee.initABP(loraSerial, devAddr, appSKey, nwkSKey, true))
   {
@@ -115,17 +164,21 @@ void setupLoRaABP(){
     debugSerial.println("Communication to LoRaBEE failed!");
   }
 }
+#endif
 
-void setupLoRaOTAA(){
+void setupLoRa(){
+#ifdef OTAA
+    //OTAA
+    setupLoRaOTAA();
+#else
+    // ABP
+    setupLoRaABP();
+#endif
+  // Uncomment this line to for the RN2903 with the Actility Network
+  // For OTAA update the DEFAULT_FSB in the library
+  // LoRaBee.setFsbChannels(1);
 
-  if (LoRaBee.initOTA(loraSerial, DevEUI, AppEUI, AppKey, true))
-  {
-    debugSerial.println("Network connection successful.");
-  }
-  else
-  {
-    debugSerial.println("Network connection failed!");
-  }
+  LoRaBee.setSpreadingFactor(9);
 }
 
 void loop()
@@ -134,41 +187,74 @@ void loop()
   int16_t temp = getTemperatureInt16();
   debugSerial.print("temperature:");
   debugSerial.println(temp/100.0);
-
+#ifdef GROVE_RGB_LCD
+  lcd.setCursor(0, 0);
+  lcd.setRGB(BLUE);
+  lcd.print("T:");
+  lcd.print(temp/100.0);
+  lcd.setCursor(0, 1);
+#endif  
 //   switch (LoRaBee.send(1, (uint8_t*)reading.c_str(), reading.length()))
     switch (LoRaBee.send(1, (uint8_t*)&temp, sizeof(temp)))
     {
     case NoError:
       debugSerial.println("Successful transmission.");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX Success");
+#endif
       break;
     case NoResponse:
       debugSerial.println("There was no response from the device.");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX NoResponse");
+#endif
       break;
     case Timeout:
       debugSerial.println("Connection timed-out. Check your serial connection to the device! Sleeping for 20sec.");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX Timeout");
+#endif
       delay(20000);
       break;
     case PayloadSizeError:
       debugSerial.println("The size of the payload is greater than allowed. Transmission failed!");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX Size Error");
+#endif
       break;
     case InternalError:
       debugSerial.println("Oh No! This shouldn't happen. Something is really wrong! The program will reset the RN module.");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX Internal Err");
+#endif
       setupLoRa();
       break;
     case Busy:
       debugSerial.println("The device is busy. Sleeping for 10 extra seconds.");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX Busy");
+#endif
       delay(10000);
       break;
     case NetworkFatalError:
       debugSerial.println("There is a non-recoverable error with the network connection. The program will reset the RN module.");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX Fatal");
+#endif
       setupLoRa();
       break;
     case NotConnected:
       debugSerial.println("The device is not connected to the network. The program will reset the RN module.");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX NotConnected");
+#endif
       setupLoRa();
       break;
     case NoAcknowledgment:
       debugSerial.println("There was no acknowledgment sent back!");
+#ifdef GROVE_RGB_LCD    
+      lcd.print("TX NoAck");
+#endif
       break;
     default:
       break;
@@ -214,6 +300,7 @@ float getTemperatureFloat()
 // TODO get GNSS module geolocation value on RX/TX pins. See http://forum.sodaq.com/search?q=GPS
 // TODO add downlink (for TX_PERIOD configuration, number of readings per transmission, low and high temperature thresholds)
 // TODO enable several readings per transmission
+// TODO add Grove LCD display
 
 
 /**
@@ -223,3 +310,57 @@ static void getHWEUI()
 {
   uint8_t len = LoRaBee.getHWEUI(DevEUI, sizeof(DevEUI));
 }
+
+
+#ifdef LED_ENABLED
+/**
+* set RGB led to red
+*/
+void ledRED() {
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
+}
+
+/**
+* set RGB led to green
+*/
+void ledGREEN() {
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, HIGH);
+}
+
+/**
+* set RGB led to blue
+*/
+void ledBLUE() {
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, LOW);
+}
+
+/**
+* set RGB led to white
+*/
+void ledWHITE() {
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, LOW);
+}
+
+/**
+* set RGB led to off
+*/
+void ledOFF() {
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
+}
+
+void setupRGBLED() {
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+}
+#endif
